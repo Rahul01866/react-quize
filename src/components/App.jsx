c7159a19fa2bec4c71bacd00a932e05e14078d28
rahul-saha <rahulbd123bd@gmail.com>
2023-07-04 20:23:56 -0700

    lession 7 source code push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,20 +11,23 @@ import Login from "./pages/login/Login";
 import Home from "./pages/signup/Home";
 import Signup from "./pages/signup/Signup";
 import "../styles/signup/App.css";
+import { AuthProvider } from "../contexts/AuthContext";
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 };
---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,72 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import "../firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const AuthContext = createContext();
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unSubscribe;
+  }, []);
+
+  // signup function
+  async function signup(email, password, username) {
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    //update profile
+    await updateProfile(auth.currentUser, {
+      displayName: username,
+    });
+
+    const user = auth.currentUser;
+    setCurrentUser({
+      ...user,
+    });
+  }
+
+  // login function
+  function login(email, password) {
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  // logout function
+  function logout() {
+    const auth = getAuth();
+    return signOut(auth);
+  }
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
