74565345515e0f016dfa09cf4dd63da40ca438bb
rahul-saha <rahulbd123bd@gmail.com>
2023-07-07 00:46:32 -0700

    lession 9 source code push

--- src/components/pages/signup/Videos.jsx
+++ src/components/pages/signup/Videos.jsx
@@ -1,30 +1,64 @@
-import classes from "../../../styles/signup/Videos.module.css";
-import Video from "./Video";
 // import { Link } from "react-router-dom";
+// import useVideoList from "../../../hooks/useVideoList";
+// import Video from "./Video";
+
+// export default function Videos() {
+//   const { loading, error, videos } = useVideoList();
+
+//   return (
+//     <div>
+//       {videos.length > 0 &&
+//         videos.map((video, index) => (
+//           <Link key={`${video.youtubeID}-${index}`} to={`/videos/${video.id}`}>
+//             <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+//           </Link>
+//         ))}
+//       {!loading && videos.length === 0 && <div>No Data Found</div>}
+//       {error && <div>There was an error</div>}
+//       {loading && <div>Loading...</div>}
+//     </div>
+//   );
+// }
+
+import { Link } from "react-router-dom";
+import useVideoList from "../../../hooks/useVideoList";
+import Video from "./Video";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Videos() {
+  const [page, setPage] = useState(1);
+  const { loading, error, videos, hasMore } = useVideoList(page);
+
   return (
-    <div className={classes.videos}>
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      {/* <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link> */}
+    <div>
+      {videos.length > 0 ? (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          next={() => setPage(page + 3)}
+        >
+          {videos.map((video, index) =>
+            video.noq > 0 ? (
+              <Link key={`${video.youtubeID}-${index}`} to="/quiz">
+                <Video
+                  title={video.title}
+                  id={video.youtubeID}
+                  noq={video.noq}
+                />
+              </Link>
+            ) : (
+              // eslint-disable-next-line react/jsx-key
+              <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+            )
+          )}
+        </InfiniteScroll>
+      ) : (
+        <div>No Data Found</div>
+      )}
+      {!loading && videos.length === 0 && <div>No Data Found</div>}
+      {error && <div>There was an error</div>}
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -7,6 +7,7 @@ const app = initializeApp({
   storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_APP_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
 });
 
 export default app;
